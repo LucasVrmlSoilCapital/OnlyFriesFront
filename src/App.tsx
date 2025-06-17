5e3b5a58c38a725dc28fe3a6296bc4773de7e843
Mathieu Van Eynde <mathieuvaneynde@Mathieus-MBP.fritz.box>
2025-06-17 14:02:13 +0200

    YOYOY

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AuthProvider } from "./contexts/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import "./App.css";
 import { NavBar } from "./components/NavBar";
 import { Order } from "./pages/Order";
 import { Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth";
+import { getUser } from "./utils/getUser";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  // useEffect(() => {
+  //   const getAuthenticatedUser = async () => {
+  //     const user = getUser();
+  //     return user;
+  //   };
+  //   getAuthenticatedUser().then((data) => {
+  //     setUser(data.user);
+  //   });
+  // }, []);
+
   return (
     <AuthProvider>
       <NavBar />
       <div className="App">
         <ProtectedRoute>
           <Routes>
+            <Route path="/" element={<Auth />} />
             <Route path="/order" element={<Order />} />
           </Routes>
         </ProtectedRoute>
---
+++ src/utils/getUser.tsx
@@ -0,0 +1,29 @@
+import { supabase } from "./supabase";
+
+export const getUser = async () => {
+  return await supabase.auth.getUser();
+};
+
+export const createSession = async (userId: string) => {
+  const { data, error } = await supabase.functions.invoke("create_session", {
+    body: { name: "Functions", user_id: userId },
+  });
+
+  if (error) {
+    throw new Error(`Error creating session: ${error.message}`);
+  }
+
+  return data;
+};
+
+export const joinSession = async (sessionId: string) => {
+  const { data, error } = await supabase.functions.invoke("join_session", {
+    body: { session_id: sessionId },
+  });
+
+  if (error) {
+    throw new Error(`Error joining session: ${error.message}`);
+  }
+
+  return data;
+};
